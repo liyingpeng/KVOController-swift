96286efcb11e7550db8cb4475fa9c259f63321a8
李应鹏 <liyingpeng@bytedance.com>
2017-07-12 11:17:08 +0800

    init

---
+++ KVOController-swift/KVOController-swiftTests/KVOController_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  KVOController_swiftTests.swift
+//  KVOController-swiftTests
+//
+//  Created by 李应鹏 on 2017/7/12.
+//  Copyright © 2017年 李应鹏. All rights reserved.
+//
+
+import XCTest
+@testable import KVOController_swift
+
+class KVOController_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
